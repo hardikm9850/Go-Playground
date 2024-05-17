6994481f1d0680c67ea80f79e9dc0ef21540be24
Hardik Mehta <hardikm9850@gmail.com>
2024-05-17 11:04:59 +0530

    add : save deck to a file

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
 
@@ -20,4 +24,19 @@ func newDeck() deck {
 		}
 	}
 	return cards
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	// handSize = 3
+	// d[:3] will return cards from index 0 to 2, that is to say it's non-inclusive
+	// d[3:] will return all cards from index 3
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) stringFormat() string {
+	return strings.Join(d, ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.stringFormat()), 0666)
 }
--- cards/main.go
+++ cards/main.go
@@ -3,13 +3,26 @@ package main
 // Deeper into Go
 // https://www.udemy.com/course/go-the-complete-developers-guide/
 func main() {
-	//cards := deck{"Ace of A", "Ace of B", "Ace of C"}
-	//cards = append(cards, "Ace of D")
+	cards := createDeck()
 
+	dealWithCards(cards)
+
+	fileSaveError := cards.saveToFile("saved_deck")
+	if fileSaveError != nil {
+		panic(fileSaveError)
+	}
+}
+
+func dealWithCards(cards deck) {
+	hand, remainingCards := deal(cards, 3)
+	println("* cards in hand*")
+	hand.print()
+	println("=====")
+	println("* cards left *")
+	remainingCards.print()
+}
+
+func createDeck() deck {
 	cards := newDeck()
-
-	cards.print()
-	// for i := 0; i < len(cards); i++ {
-	//println(i, cards[i])
-	// }
+	return cards
 }
