5b4c80fc6745acab82b7b9e12b887a286e84f90c
Hardik <hardikm9850@gmail.com>
2024-06-26 12:32:40 +0530

    Update Map literal.go

--- GoTour/Map/Map literal.go
+++ GoTour/Map/Map literal.go
@@ -10,21 +10,11 @@ type Vertex struct {
 	Lat, Long float64
 }
 
+//If the top-level type is just a type name, in our case Vertex, you can omit it from the elements of the literal.
 var m = map[string]Vertex{
-	"Bell Labs": Vertex{
-		40.68433, -74.39967,
-	},
-	"Google": Vertex{
-		37.42202, -122.08408,
-	},
+	"Bell Labs": {40.68433, -74.39967},
+	"Google":    {37.42202, -122.08408},
 }
-
-func main() {
-	fmt.Println(m)
-	fmt.Println(mapLiteral)
-}
-
-
 var mapLiteral = map[string]Vertex {
 	"Pune" : Vertex {
 		1.1 , 2.2,
@@ -32,4 +22,9 @@ var mapLiteral = map[string]Vertex {
 	"BLR" : Vertex {
 		21.1 , 32.2,
 	},
+}
+
+func main() {
+	fmt.Println(m)
+	fmt.Println(mapLiteral)
 }
