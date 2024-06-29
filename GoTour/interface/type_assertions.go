e06b5605a097e4b4bfc3cbef40dae84518a8bd59
Hardik <hardikm9850@gmail.com>
2024-06-29 17:51:29 +0530

    Create type_assertions.go

---
+++ GoTour/interface/type_assertions.go
@@ -0,0 +1,45 @@
+/**
+type assertion provides access to an interface value's underlying concrete value.
+
+t := i.(T)
+This statement asserts that the interface value i holds the concrete type T and assigns the underlying T value to the variable t.
+
+If i does not hold a T, the statement will trigger a panic.
+
+To test whether an interface value holds a specific type, a type assertion can return two values: the underlying value and a boolean value that reports whether the assertion succeeded.
+
+t, ok := i.(T)
+If i holds a T, then t will be the underlying value and ok will be true.
+
+If not, ok will be false and t will be the zero value of type T, and no panic occurs.
+
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var i interface{} = "hello"
+
+	s := i.(string)
+	fmt.Println(s)
+
+	s, ok := i.(string)
+	fmt.Println(s, ok)
+
+	f, ok := i.(float64)
+	fmt.Println(f, ok)
+
+	f = i.(float64) // panic
+	fmt.Println(f)
+}
+
+// ------- output -------
+
+hello
+hello true
+0 false
+panic: interface conversion: interface {} is string, not float64
+
+goroutine 1 [running]:
