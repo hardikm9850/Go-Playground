0d2d04873ba2c85c8723c1a5fcb88ba58f97f7e6
Hardik <hardikm9850@gmail.com>
2024-06-30 17:05:06 +0530

    Create stringer.go

---
+++ GoTour/interface/stringer.go
@@ -0,0 +1,23 @@
+/**
+A Stringer is a type that can describe itself as a string. This is same as our toString() method we override in POJO classes 
+to represent instance as a string representation
+*/
+
+package main
+
+import "fmt"
+
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p Person) String() string {
+	return fmt.Sprintf("%v (%v years)", p.Name, p.Age)
+}
+
+func main() {
+	a := Person{"Arthur Dent", 42}
+	z := Person{"Zaphod Beeblebrox", 9001}
+	fmt.Println(a, z)
+}
